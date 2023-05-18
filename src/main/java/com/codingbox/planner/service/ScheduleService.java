e98a99b39271c81d3a90a5d9eb87ddb0a7611492
SeungHoon Kang <duoogo@gmail.com>
2023-05-18 10:47:36 +0900

    2023-05-18 여행 일정 페이지에 달력 기능, 여행 일정 추가 기능, 로그인 확인 기능 구현

---
+++ src/main/java/com/codingbox/planner/service/ScheduleService.java
@@ -0,0 +1,38 @@
+package com.codingbox.planner.service;
+
+import com.codingbox.planner.domain.Members;
+import com.codingbox.planner.domain.Schedule;
+import com.codingbox.planner.Repository.ScheduleRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ScheduleService {
+    private final ScheduleRepository scheduleRepository;
+
+    public ScheduleService(ScheduleRepository scheduleRepository) {
+        this.scheduleRepository = scheduleRepository;
+    }
+
+    public Schedule createSchedule(Schedule schedule) {
+        return scheduleRepository.save(schedule);
+    }
+
+    public Optional<Schedule> getScheduleById(Long id) {
+        return scheduleRepository.findById(id);
+    }
+
+    public List<Schedule> getAllSchedules() {
+        return scheduleRepository.findAll();
+    }
+
+    public void deleteSchedule(Long id) {
+        scheduleRepository.deleteById(id);
+    }
+
+    public List<Schedule> getScheduleByMembers(Members members) {
+        return scheduleRepository.findByMembersToSchedule(members);
+    }
+}
