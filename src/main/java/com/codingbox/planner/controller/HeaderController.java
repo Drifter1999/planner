83444434520b7057aaf93e3a5b8c4f3365141d03
SangGi <yah9600@gmail.com>
2023-05-22 21:46:58 +0900

    2023-05-22 category에서 컨텐츠 담은 후 일정으로 이동

--- src/main/java/com/codingbox/planner/controller/HeaderController.java
+++ src/main/java/com/codingbox/planner/controller/HeaderController.java
@@ -1,17 +1,24 @@
 package com.codingbox.planner.controller;
 
 import com.codingbox.planner.domain.DTO.AreaDTO;
+import com.codingbox.planner.domain.DTO.ScheduleCartDTO;
+import com.codingbox.planner.domain.DTO.ScheduleDTO;
 import com.codingbox.planner.domain.DTO.SearchKeyWordDTO;
 import com.codingbox.planner.service.ListingService;
 import com.codingbox.planner.service.MultiSelectService;
+import com.codingbox.planner.service.ScheduleCartService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -20,6 +27,8 @@ import java.util.List;
 public class  HeaderController {
     private final ListingService listingService;
     private final MultiSelectService multiSelectService;
+
+    private final ScheduleCartService scheduleCartService;
 
     @GetMapping("/about")
     public String about() {
@@ -69,8 +78,32 @@ public class  HeaderController {
         return "redirect:/listing/details";
     }
 
-    @GetMapping("/Calendar")
-    public String Calendar() {
+    @PostMapping("/Calendar")
+    public String Calendar(@RequestParam("startDate") String startDate,
+                           @RequestParam("endDate") String endDate,
+                           @RequestParam("contentTitleList") String contentTitleList,
+                           @RequestParam("ContentType") String ContentType,
+                           Model model) throws ParseException {
+        contentTitleList = contentTitleList.substring(0, contentTitleList.length()-2);
+        contentTitleList = contentTitleList.replaceAll("\"", "");
+        String contentName = "";
+        switch (ContentType) {
+            case "12" : contentName = "관광지"; break;
+            case "28" : contentName = "레포츠"; break;
+            case "32" : contentName = "숙박"; break;
+            case "39" : contentName = "음식점"; break;
+            default:
+                System.out.println("잘못된 값입니다.");
+        }
+
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-mm-dd");
+        Date date1 = formatter.parse(startDate);
+        Date date2 = formatter.parse(endDate);
+
+        List<ScheduleCartDTO> CartArr = scheduleCartService.createCartList(date1, date2, contentTitleList, contentName);
+
+        model.addAttribute("cartArr", CartArr);
+
         return "Calendar";
     }
 
---
+++ src/main/java/com/codingbox/planner/service/ScheduleCartService.java
@@ -0,0 +1,27 @@
+package com.codingbox.planner.service;
+
+import com.codingbox.planner.domain.DTO.ScheduleCartDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class ScheduleCartService {
+    public List<ScheduleCartDTO> createCartList (Date startDate, Date endDate, String contentTitleList, String contentName) {
+        List<ScheduleCartDTO> CartArr = new ArrayList<ScheduleCartDTO>();
+        String [] str = contentTitleList.split(",");
+
+        for (int i = 0 ; i < str.length ; i++) {
+            ScheduleCartDTO data = new ScheduleCartDTO();
+            data.setStrDate(startDate);
+            data.setEndDate(endDate);
+            data.setContentTitleList(str[i]);
+            data.setContentName(contentName);
+
+            CartArr.add(data);
+        }
+        return CartArr;
+    }
+}
